0f2cbd755fa6c2ea971a14b5b75263adbbaecca8
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:56:31 +0600

    GlobalData selectors

---
+++ src/redux/GlobalData/GlobalData.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect'
+
+const selectGlobalDataState = (state) => state.globalData
+
+export const selectGlobalData = createSelector(
+  [selectGlobalDataState],
+  (data) => data.data
+)
+
+export const selectIsGDFetching = createSelector(
+  [selectGlobalDataState],
+  (globalData) => globalData.isFetching
+)
+
+export const isGDLoaded = createSelector(
+  [selectGlobalDataState],
+  (globalData) => !!globalData.data
+)
