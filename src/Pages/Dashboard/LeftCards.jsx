3ba0cc09669df3693835bfd33d9e1e4fe35d9690
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:50:51 +0600

    Dashboard component for left cards

---
+++ src/Pages/Dashboard/LeftCards.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { Grid, Card } from '@material-ui/core'
+import { createStructuredSelector } from 'reselect'
+import { connect } from 'react-redux'
+import { fetchData } from '../../api/mathdroAPI'
+import {
+  selectGlobalData,
+  selectIsGDFetching,
+  isGDLoaded,
+} from '../../redux/GlobalData/GlobalData.selectors'
+import InfoCard from '../../shared/InfoCard/InfoCard'
+const LeftCards = ({ globalData, gdFetching, gdLoaded }) => {
+  const [CD, setCD] = useState(null)
+  const [CDFetching, setCDFetching] = useState(false)
+
+  useEffect(() => {
+    setCDFetching(true)
+    const fetchApi = async () => {
+      const data = await fetchData('Bangladesh')
+      setCD(data)
+      setCDFetching(false)
+    }
+    fetchApi()
+    //eslint-disable-next-line
+  }, [])
+
+  return (
+    <Grid component={Card} elevation={0} item md={3} xs={12} lg={3}>
+      <InfoCard
+        title='Global'
+        color='red'
+        data={globalData}
+        loading={gdFetching}
+        loaded={gdLoaded}
+      />
+      <InfoCard
+        title='Bangladesh'
+        color='green'
+        data={CD}
+        loading={CDFetching}
+        loaded={!!CD}
+      />
+    </Grid>
+  )
+}
+const mapStateToProps = createStructuredSelector({
+  globalData: selectGlobalData,
+  gdFetching: selectIsGDFetching,
+  gdLoaded: isGDLoaded,
+})
+export default connect(mapStateToProps)(LeftCards)
