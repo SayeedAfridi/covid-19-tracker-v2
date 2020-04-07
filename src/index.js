2bbe1b689039be881ad96cfaa14849d810877d38
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:51:28 +0600

    added redux provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
 serviceWorker.unregister()
