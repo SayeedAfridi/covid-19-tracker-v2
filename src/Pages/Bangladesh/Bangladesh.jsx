587a9ee31ceda2e6b7e3510428073d25cbafaa3c
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 00:41:25 +0600

    page placeholder

---
+++ src/Pages/Bangladesh/Bangladesh.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Bangladesh = () => {
+  return <div>Hello from Bangladesh</div>
+}
+
+export default Bangladesh
---
+++ src/Pages/News/News.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const News = () => {
+  return <div>Hello From News</div>
+}
+
+export default News
