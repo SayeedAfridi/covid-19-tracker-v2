8d8c86aea77c9913e8f804d4dd88d9b9ed9d2b6d
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:56:04 +0600

    GlobalData actions

---
+++ src/redux/GlobalData/GlobalData.actions.js
@@ -0,0 +1,30 @@
+import GlobalDataActionTypes from './GlobalData.types'
+import { fetchData } from '../../api/mathdroAPI'
+export const fetchGlobalDataStart = () => ({
+  type: GlobalDataActionTypes.FETCH_GLOBALDATA_START,
+})
+
+export const fetchGlobalDataSuccess = (data) => ({
+  type: GlobalDataActionTypes.FETCH_GLOBALDATA_SUCCESS,
+  payload: data,
+})
+
+export const fetchGlobalDataFailure = (errorMessage) => ({
+  type: GlobalDataActionTypes.FETCH_GLOBALDATA_FAILURE,
+  payload: errorMessage,
+})
+
+export const fetchGlobalDataStartAsync = () => {
+  return (dispatch) => {
+    dispatch(fetchGlobalDataStart())
+    const FetchApi = async () => {
+      const data = await fetchData()
+      if (data.message) {
+        dispatch(fetchGlobalDataFailure(data.message))
+      } else {
+        dispatch(fetchGlobalDataSuccess(data))
+      }
+    }
+    FetchApi()
+  }
+}
