5d71b116de8d98d3cfaeb926b78c492599be45c2
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 04:06:58 +0600

    chart for dailydata

---
+++ src/shared/Charts/LineChart/LineChart.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { Line } from 'react-chartjs-2'
+import {
+  selectDailyData,
+  selectIsDDFetching,
+  isDDLoaded,
+} from '../../../redux/DailyData/DailyData.selectors'
+import Loader from '../../loader/loader'
+import FadeElement from '../../FadeElement/FadeElement'
+
+const LineChart = ({ dailyData, ddFetching }) => {
+  return (
+    <>
+      <FadeElement enter={ddFetching}>
+        <Loader text='fetching data' />
+      </FadeElement>
+      <FadeElement enter={!ddFetching}>
+        <Line
+          data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [
+              {
+                data: dailyData.map(({ confirmed }) => confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+              },
+              {
+                data: dailyData.map(({ deaths }) => deaths),
+                label: 'Deaths',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true,
+              },
+            ],
+          }}
+        />
+      </FadeElement>
+    </>
+  )
+}
+
+const mapStateToProps = createStructuredSelector({
+  dailyData: selectDailyData,
+  ddFetching: selectIsDDFetching,
+  ddLoaded: isDDLoaded,
+})
+
+export default connect(mapStateToProps)(LineChart)
