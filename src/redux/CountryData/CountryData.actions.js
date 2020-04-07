7623ed7ff7e56bd256dc4ffc54b18048956fc537
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:54:24 +0600

    CoutryData actions

---
+++ src/redux/CountryData/CountryData.actions.js
@@ -0,0 +1,31 @@
+import CountryDataActionTypes from './CountryData.types'
+import { fetchData } from '../../api/mathdroAPI'
+
+export const fetchCountryDataStart = () => ({
+  type: CountryDataActionTypes.FETCH_COUNTRYDATA_START,
+})
+
+export const fetchCountryDataSuccess = (data) => ({
+  type: CountryDataActionTypes.FETCH_COUNTRYDATA_SUCCESS,
+  payload: data,
+})
+
+export const fetchCountryDataFailure = (errorMessage) => ({
+  type: CountryDataActionTypes.FETCH_COUNTRYDATA_FAILURE,
+  payload: errorMessage,
+})
+
+export const fetchCountryDataStartAsync = (country) => {
+  return (dispatch) => {
+    dispatch(fetchCountryDataStart())
+    const FetchApi = async () => {
+      const data = await fetchData(country)
+      if (data.message) {
+        dispatch(fetchCountryDataFailure(data.message))
+      } else {
+        dispatch(fetchCountryDataSuccess(data))
+      }
+    }
+    FetchApi()
+  }
+}
