efde07a337eb4d75cf9096764e6b4da1061aaadd
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 00:41:03 +0600

    export all pages

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export { default as About } from './About/About'
+export { default as Dashboard } from './Dashboard/Dashboard'
+export { default as News } from './News/News'
+export { default as Bangladesh } from './Bangladesh/Bangladesh'
