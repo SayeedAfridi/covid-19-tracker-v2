f28da1d851d246956ee0266ebe8a1829bdca872e
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 02:55:19 +0600

    DailyData reducer

---
+++ src/redux/DailyData/DailyData.reducer.js
@@ -0,0 +1,35 @@
+import DailyDataActionTypes from './DailyData.types'
+
+const INITIAL_STATE = {
+  data: null,
+  isFetching: false,
+  errorMessage: undefined,
+}
+
+const DailyDataReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case DailyDataActionTypes.FETCH_DAILYDATA_START:
+      return {
+        ...state,
+        isFetching: true,
+      }
+
+    case DailyDataActionTypes.FETCH_DAILYDATA_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+      }
+
+    case DailyDataActionTypes.FETCH_DAILYDATA_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export default DailyDataReducer
