4856cda7937c72edb001314d04d8959622473060
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 00:39:27 +0600

    added navigations and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { About, News, Dashboard, Bangladesh } from './Pages'
+import Navigation from './Navigation/Navigation'
 const App = () => {
-  return <div>Hello World..!</div>
+  return (
+    <>
+      <Navigation />
+      <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/news' component={News} />
+        <Route exact path='/bd' component={Bangladesh} />
+        <Redirect to='/' />
+      </Switch>
+    </>
+  )
 }
 
 export default App
