b82ec7e7e65a2323ecc1a1721aa59b85439d0ee6
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-08 22:40:50 +0600

    compatile for dark theme

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Card } from '@material-ui/core'
+import { Grid, Card, useTheme } from '@material-ui/core'
 import { fetchGlobalDataStartAsync } from '../../redux/GlobalData/GlobalData.actions'
 import LeftCards from './LeftCards'
 import LineChart from '../../shared/Charts/LineChart/LineChart'
@@ -9,7 +9,7 @@ import BarChart from '../../shared/Charts/BarChart/BarChart'
 import RightCards from './RightCards'
 const useStyles = makeStyles((theme) => ({
   container: {
-    margin: '0px 1.5%',
+    width: '100%',
   },
   card: {
     marginBottom: '5px',
@@ -27,27 +27,31 @@ const useStyles = makeStyles((theme) => ({
 }))
 const Dashboard = ({ fetchGlobalData, fetchCountryData }) => {
   const classes = useStyles()
+  const theme = useTheme()
 
   useEffect(() => {
     fetchGlobalData()
     //eslint-disable-next-line
   }, [])
-  //const {confirmed, recovered, deaths, lastupdate} = globalData
   return (
-    <div className={classes.container}>
-      <Grid container spacing={3}>
-        <LeftCards fetchCountry={fetchCountryData} />
-        <Grid item md={6} xs={12} lg={6}>
-          <Card className={classes.chartCard} style={{ position: 'relative' }}>
-            <LineChart />
-          </Card>
-          <Card className={classes.chartCard} style={{ position: 'relative' }}>
-            <BarChart />
-          </Card>
-        </Grid>
-        <RightCards />
+    <Grid
+      container
+      spacing={2}
+      style={{
+        background: theme.palette.type === 'light' ? '#fff' : '#3a3a4f',
+      }}
+    >
+      <LeftCards fetchCountry={fetchCountryData} />
+      <Grid item md={6} xs={12} lg={6}>
+        <Card className={classes.chartCard} style={{ position: 'relative' }}>
+          <LineChart />
+        </Card>
+        <Card className={classes.chartCard} style={{ position: 'relative' }}>
+          <BarChart />
+        </Card>
       </Grid>
-    </div>
+      <RightCards />
+    </Grid>
   )
 }
 
