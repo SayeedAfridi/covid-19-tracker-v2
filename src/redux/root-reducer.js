cf2fb8d0417819ba29737e1399507676284ad8a7
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-10 17:38:44 +0600

    Remove Countries Reducer and all its actions.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux'
 import GlobalDataReducer from './GlobalData/GlobalData.reducer'
 import CountryDataReducer from './CountryData/CountryData.reducer'
 import DailyDataReducer from './DailyData/DailyData.reducer'
-import CountriesReducer from './Countries/Countries.reducer'
 const rootReducer = combineReducers({
   globalData: GlobalDataReducer,
   countryData: CountryDataReducer,
   dailyData: DailyDataReducer,
-  countries: CountriesReducer,
 })
 
 export default rootReducer
