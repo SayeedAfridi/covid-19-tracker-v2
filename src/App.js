d11d51651ac35808fdf6f9443bfcbad5c3ec57c8
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-10 17:32:52 +0600

    fetch global data from itVanilla API

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import { fetchDailyDataStartAsync } from './redux/DailyData/DailyData.actions'
 import { fetchCountryDataStartAsync } from './redux/CountryData/CountryData.actions'
 import { fetchCountriesStartAsync } from './redux/Countries/Countries.actions'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { fetchGlobalDataStartAsync } from './redux/GlobalData/GlobalData.actions'
 import theme, { lightTheme } from './shared/theme'
-const App = ({ fetchDailyData, fetchBDData, fetchCountries }) => {
+const App = ({ fetchDailyData, fetchGD, fetchBDData }) => {
   const localTheme = localStorage.getItem('theme')
   const defaultTheme = localTheme === 'light' ? lightTheme : theme
   const [theTheme, setTheme] = useState(defaultTheme)
@@ -22,7 +23,7 @@ const App = ({ fetchDailyData, fetchBDData, fetchCountries }) => {
     }
   }
   useEffect(() => {
-    fetchCountries()
+    fetchGD()
     fetchDailyData()
     fetchBDData('Bangladesh')
     //eslint-disable-next-line
@@ -41,6 +42,7 @@ const App = ({ fetchDailyData, fetchBDData, fetchCountries }) => {
   )
 }
 const mapDispatchToProps = (dispatch) => ({
+  fetchGD: () => dispatch(fetchGlobalDataStartAsync()),
   fetchDailyData: () => dispatch(fetchDailyDataStartAsync()),
   fetchBDData: (country) => dispatch(fetchCountryDataStartAsync(country)),
   fetchCountries: () => dispatch(fetchCountriesStartAsync()),
