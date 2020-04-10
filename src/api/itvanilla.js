41ec808404fe96e1da7c3778e4b67baa27927de2
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-10 17:36:28 +0600

    It vanila api for fetching latest data

---
+++ src/api/itvanilla.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+const url = 'https://api.itvanilla.com/covid19'
+
+export const fetchData = async (country) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(url)
+    if (country) {
+      const countryData = data.table[0].filter((el) => el.Country === country)
+      return {
+        confirmed: parseInt(countryData[0].TotalCases.split(',').join('')),
+        deaths: parseInt(countryData[0].TotalDeaths.split(',').join('')),
+        recovered: parseInt(countryData[0].TotalRecovered.split(',').join('')),
+        newCases: parseInt(countryData[0].NewCases.split(',').join('')) || 0,
+        newDeaths: parseInt(countryData[0].NewDeaths.split(',').join('')) || 0,
+      }
+    }
+    const newCases =
+      parseInt(data.table[0][0].NewCases.split(',').join('')) || 0
+    const newDeaths =
+      parseInt(data.table[0][0].NewDeaths.split(',').join('')) || 0
+
+    const newData = {
+      confirmed: data.cases,
+      deaths: data.deaths,
+      recovered: data.recovered,
+      lastUpdate: Date.now(),
+      flag: data.flag,
+      newCases,
+      newDeaths,
+      table: data.table && data.table[0],
+    }
+    return newData
+  } catch (err) {
+    return err
+  }
+}
