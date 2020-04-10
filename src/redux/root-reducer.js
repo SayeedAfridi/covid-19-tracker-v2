08eb65fceae618be705e6e1f6463fcf2c97c4f88
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-11 03:37:12 +0600

    Alldata reduces

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import GlobalDataReducer from './GlobalData/GlobalData.reducer'
 import CountryDataReducer from './CountryData/CountryData.reducer'
 import DailyDataReducer from './DailyData/DailyData.reducer'
+import AllDataReducer from './AllData/AllData.reducer'
 const rootReducer = combineReducers({
   globalData: GlobalDataReducer,
   countryData: CountryDataReducer,
   dailyData: DailyDataReducer,
+  allData: AllDataReducer,
 })
 
 export default rootReducer
