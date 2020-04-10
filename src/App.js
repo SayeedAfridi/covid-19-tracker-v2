02966c2bca9a767feac995793e974b65e08a55bc
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-11 03:34:55 +0600

    Disable development routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { About, News, Dashboard, Bangladesh } from './Pages'
+import { Dashboard, Map } from './Pages'
 import Navigation from './Navigation/Navigation'
 import { connect } from 'react-redux'
 import { fetchDailyDataStartAsync } from './redux/DailyData/DailyData.actions'
 import { fetchCountryDataStartAsync } from './redux/CountryData/CountryData.actions'
-import { fetchCountriesStartAsync } from './redux/Countries/Countries.actions'
+import { fetchAllDataStartAsync } from './redux/AllData/AllData.actions'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { fetchGlobalDataStartAsync } from './redux/GlobalData/GlobalData.actions'
 import theme, { lightTheme } from './shared/theme'
-import Map from './Pages/Map/Map'
-const App = ({ fetchDailyData, fetchGD, fetchBDData }) => {
+const App = ({ fetchDailyData, fetchGD, fetchBDData, fetchAll }) => {
   const localTheme = localStorage.getItem('theme')
   const defaultTheme = localTheme === 'light' ? lightTheme : theme
   const [theTheme, setTheme] = useState(defaultTheme)
@@ -26,6 +25,7 @@ const App = ({ fetchDailyData, fetchGD, fetchBDData }) => {
   useEffect(() => {
     fetchGD()
     fetchDailyData()
+    fetchAll()
     fetchBDData('Bangladesh')
     //eslint-disable-next-line
   }, [])
@@ -35,9 +35,9 @@ const App = ({ fetchDailyData, fetchGD, fetchBDData }) => {
       <Switch>
         <Route exact path='/' component={Dashboard} />
         <Route exact path='/map' component={Map} />
-        <Route exact path='/about' component={About} />
+        {/* <Route exact path='/about' component={About} />
         <Route exact path='/news' component={News} />
-        <Route exact path='/bd' component={Bangladesh} />
+        <Route exact path='/bd' component={Bangladesh} /> */}
         <Redirect to='/' />
       </Switch>
     </MuiThemeProvider>
@@ -47,6 +47,6 @@ const mapDispatchToProps = (dispatch) => ({
   fetchGD: () => dispatch(fetchGlobalDataStartAsync()),
   fetchDailyData: () => dispatch(fetchDailyDataStartAsync()),
   fetchBDData: (country) => dispatch(fetchCountryDataStartAsync(country)),
-  fetchCountries: () => dispatch(fetchCountriesStartAsync()),
+  fetchAll: () => dispatch(fetchAllDataStartAsync()),
 })
 export default connect(null, mapDispatchToProps)(App)
