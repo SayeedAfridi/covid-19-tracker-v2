b8bbcdbf9bf3e899e640445e6e9bd1a48661a763
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-13 03:51:35 +0600

    uncomment news route

--- src/Navigation/NavLinks.js
+++ src/Navigation/NavLinks.js
@@ -11,10 +11,10 @@ export const navLinks = [
   //   name: 'bangladesh',
   //   path: '/bd',
   // },
-  // {
-  //   name: 'news',
-  //   path: '/news',
-  // },
+  {
+    name: 'news',
+    path: '/news',
+  },
   // {
   //   name: 'about',
   //   path: '/about',
